613727d030c18b059fa9f1389d0440551c0c8632
yanrodrigues205 <yan.pablo205@gmail.com>
2023-07-15 12:30:08 -0300

    add model sessao novamente

---
+++ Models/Sessao.php
@@ -0,0 +1,54 @@
+<?php
+
+class Sessao
+{
+    private static $usuario_logado = null;
+
+
+
+    public function __construct()
+    {
+        Sessao::iniciar();
+    }
+
+    public function setarSessao($usuario){
+        if($usuario)
+        {
+            Sessao::setar(Sessao::$usuario_logado, $usuario->pegarID());
+            return;
+        }
+
+        Sessao::remover(Sessao::$usuario_logado);
+    } 
+
+    private static function iniciar()
+    {
+        session_start();
+    }
+
+    private static function setar($chave, $valor)
+    {
+        $_SESSION[$chave] = $valor;
+    }
+
+    private static function pegar($chave)
+    {
+        if(isset($_SESSION[$chave]))
+        {
+            return $_SESSION[$chave];
+        }
+        else
+        {
+            return $_SESSION[$key] = null;
+        }
+    }
+
+    private static function remover()
+    {
+        unset($_SESSION[$key]);
+        session_destroy($_SESSION[$key]);
+    }
+
+}
+
+?>
