cef03bd37ea357665679b52583cec7fb5111297a
yanrodrigues205 <yan.pablo205@gmail.com>
2023-07-17 23:09:53 -0300

    iniciando inclusão de produtos no back-end

--- Controllers/MenuController.php
+++ Controllers/MenuController.php
@@ -3,15 +3,19 @@
 require_once "AuthenticaController.php";
 
 class MenuController extends AuthenticaController{
+
+
     public function __construct(){
-        AuthenticaController::verificacao();
     }
 
     public function iniciar(){        
+        $dados_usuario = AuthenticaController::verificacao();   
         $view_caminho = "./Views/privado/dashboard/index.php";
         include_once $view_caminho;
 
     }
+
+    
 }
 
 ?>
---
+++ Controllers/ProdutoController.php
@@ -0,0 +1,17 @@
+<?php
+
+    require_once("BancoController.php");
+
+    class ProdutoController extends BancoController
+    {
+
+        public function __construct()
+        {
+
+        }
+
+        
+
+    }
+
+?>
