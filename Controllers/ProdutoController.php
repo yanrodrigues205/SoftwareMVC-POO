8b5a8d34249f7d695bfdf28be169ea20d8478f1c
yanrodrigues205 <yan.pablo205@gmail.com>
2023-07-18 21:44:18 -0300

    adicionando autheticacao a pagina

--- Controllers/ProdutoController.php
+++ Controllers/ProdutoController.php
@@ -1,5 +1,5 @@
 <?php
-
+    require_once("AuthenticaController.php");
     require_once("BancoController.php");
     require_once("Models/Produto.php");
 
@@ -8,12 +8,14 @@
 
         public function __construct()
         {
-
+            $authentica = new AuthenticaController();
+            $verifica = $authentica->verificacao();
         }
 
         public function cadastrar(){
             $conexao = $this->conexaoBD();
             $view_caminho = "./Views/privado/produtos/cadastrar.php";
+            
 
             if(!empty($_POST['titulo']) && !empty($_POST['descricao']) && !empty($_POST['quantidade']) && !empty($_POST['tipo']) && !empty($_POST['valor'])){
                 $produto = new Produto($_POST['titulo'], $_POST['descricao'], $_POST['valor'], $_POST['quantidade'], $_POST['tipo']);
