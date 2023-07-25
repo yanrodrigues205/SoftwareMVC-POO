cd02a1b852356905777df537bb688750ce757dd0
yanrodrigues205 <yan.pablo205@gmail.com>
2023-07-24 22:57:17 -0300

    valores da documentacao PIX na model PagamentoPix

--- Models/PagamentoPix.php
+++ Models/PagamentoPix.php
@@ -6,19 +6,20 @@ class PagamentoPix{
     /** IDs DO PAYLOAD PIX - DESTINATARIO
      * @var string
      */
+    //VALORES PRE SETADOS COM BASE NA DOCUMENTACAO DO BANCO CENTRAL
         const ID_PAYLOAD_FORMAT_INDICATION = "00";
-        const ID_MERCHANT_ACCOUNT_INFORMATION = "00";
+        const ID_MERCHANT_ACCOUNT_INFORMATION = "25";
         const ID_MERCHANT_ACCOUNT_INFORMATION_GUI = "00";
-        const ID_MERCHANT_ACCOUNT_INFORMATION_KEY = "00";
-        const ID_MERCHANT_ACCOUNT_INFORMATION_DESCRIPTION = "00";
-        const ID_MERCHANT_CATEGORY_CODE = "00";
-        const ID_TRANSACTION_CURRENCY = "00";
-        const ID_TRANSACTION_AMOUNT = "00";
-        const ID_COUNTRY_CODE = "00";
-        const ID_CMERCHANT_NAME = "00";
-        const ID_CMERCHANT_CITY = "00";
-        const ID_ADDITIONAL_DATA_FIELD_TEMPLATE = "00";
-        const ID_ADDITIONAL_DATA_FIELD_TEMPLATE_TXID = "00";
+        const ID_MERCHANT_ACCOUNT_INFORMATION_KEY = "01";
+        const ID_MERCHANT_ACCOUNT_INFORMATION_DESCRIPTION = "02";
+        const ID_MERCHANT_CATEGORY_CODE = "52";
+        const ID_TRANSACTION_CURRENCY = "53";
+        const ID_TRANSACTION_AMOUNT = "54";
+        const ID_COUNTRY_CODE = "58";
+        const ID_CMERCHANT_NAME = "59";
+        const ID_CMERCHANT_CITY = "60";
+        const ID_ADDITIONAL_DATA_FIELD_TEMPLATE = "62";
+        const ID_ADDITIONAL_DATA_FIELD_TEMPLATE_TXID = "05";
         const ID_CRC16 = "16";
    
 
@@ -60,6 +61,12 @@ class PagamentoPix{
     public function setValor($valor){
         $this->valor_transacao = $valor;
         return $this;
+    }
+
+    private function pegarValor($id, $valor){
+        //CADA ATRIBUTO DA CLASSE DEVE PASSAR POR ESTE METODO, POIS ELE E RESPONSAVEL POR TRANFORMAR DE FORMA QUE O PIX IDENTIFIQUE
+        $tamanho = str_pad(strlen($valor), 2, '0', STR_PAD_LEFT);
+        return $id.$tamanho.$valor;
     }
 
 
